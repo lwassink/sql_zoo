02ef7bad85c383fb18e0123163d6bcf4ce9db2f3
App Academy Student <student@appacademy.io>
2016-09-26 14:33:37 -0700

    finish 05

--- lib/05_aggregates.rb
+++ lib/05_aggregates.rb
@@ -44,18 +44,36 @@ end
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
+    SELECT
+      COUNT(*)
+    FROM
+      countries
+    WHERE
+      area > 1000000
   SQL
 end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
   execute(<<-SQL)
+    SELECT
+      SUM(population)
+    FROM
+      countries
+    WHERE
+      name IN ('France','Germany','Spain')
   SQL
 end
 
 def country_counts
   # For each continent show the continent and number of countries.
   execute(<<-SQL)
+    SELECT
+      continent, COUNT(countries)
+    FROM
+      countries
+    GROUP BY
+      continent
   SQL
 end
 
@@ -63,11 +81,27 @@ def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
   execute(<<-SQL)
+    SELECT
+      continent, COUNT(countries)
+    FROM
+      countries
+    WHERE
+      population >= 10000000
+    GROUP BY
+      continent
   SQL
 end
 
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
+    SELECT
+      continent
+    FROM
+      countries
+    GROUP BY
+      continent
+    HAVING
+      SUM(population) >= 100000000
   SQL
 end
